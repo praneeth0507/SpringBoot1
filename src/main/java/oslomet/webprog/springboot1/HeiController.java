d5285200229a4fac005e2f9d97c9c8bdbb8c343b
praneeth0507 <114682286+praneeth0507@users.noreply.github.com>
2023-04-10 13:17:18 +0200

    Initial commit

---
+++ src/main/java/oslomet/webprog/springboot1/HeiController.java
@@ -0,0 +1,15 @@
+package oslomet.webprog.springboot1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeiController {
+    @GetMapping("/")
+    public String hei(String navn){
+        int antallBokstaver = navn.length();
+        return "Hei verden " + navn + " ! Navnet ditt er " + antallBokstaver + " langt.";
+    }
+
+
+}
